bdf8683812fe47786970106a8dad2fd9f1e590cc
MonxPoke <35337382+Poider@users.noreply.github.com>
2022-03-23 12:44:44 +0100

    Update exec.c

--- exec.c
+++ exec.c
@@ -36,50 +36,48 @@ char **args_to_table(t_arguments *args)
 int execute(t_line_processing_history *v_lines)
 {
 	t_commandtable *command_table;
-	int id;
 	char **args;
 	int fd[2];
 	int pipe_save;
+	int out;
 
 	pipe_save = 0;
 	command_table = v_lines->command_table;
 	while (command_table != 0)
 	{
 		pipe(fd);
-		
-		id = fork();
-		if (id == 0)
+		out = fd[1];
+		v_lines->process_id = fork();
+		if (v_lines->process_id == 0)
 		{
+			close(fd[0]);
 			if (pipe_save != 0)
 				dup2(pipe_save, 0);
 			if(command_table -> input_fd)
 				dup2(command_table->input_fd, 0);
-			dup2(command_table->output_fd, 1);
+			if (command_table -> output_fd >= 3)
+			{
+				out = command_table -> output_fd;
+			}
+			else if (command_table ->next == 0)
+				out = 1;
+			dup2(out, 1);
 			args = args_to_table(command_table->argument);
 			// complete
 			// check access and execute built in or excve if its not in them
 			// for excve args is argument table
 			// v_lines->env_vars is env vars table
 			// v_lines->exit_status is where to put exit status from waitpid
-			free(args);
-			close(fd[1]);
-			close(fd[0]);
-			if (pipe_save != 0 && pipe_save != 1)
-				close(pipe_save);
-			close_fds(v_lines->command_table);
 			return 1;
 		}
 		if (pipe_save != 0 && pipe_save != 1)
 			close(pipe_save);
 
-		//wait(NULL)?!
-
 		close(fd[1]);
-		if (command_table->output_fd == 1)
-			pipe_save = fd[0];
-		else
-			pipe_save = 0;
+		pipe_save = fd[0];
 		command_table = command_table->next;
 	}
+	
+	//wait all chilldreen 
 	return 1;
 }
